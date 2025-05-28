951822cea4455841e76f4a6c2ea6bf0b4a604ace
leylaheydarova <lbadalzade@gmail.com>
2025-05-28 23:47:58 +0400

    Little sample for OAuth

---
+++ OAuth/OAuthClient/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace OAuthClient.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/callback")]
+        public async Task<IActionResult> Callback(string code, string state)
+        {
+            var client = new HttpClient();
+            var response = await client.PostAsync("https://localhost:5001/oauth/token", new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("client_id", "client_app")
+            }));
+
+            var content = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonSerializer.Deserialize<Dictionary<string, object>>(content);
+
+            return Content($"Access Token: {tokenResponse["access_token"]}");
+        }
+    }
+}
